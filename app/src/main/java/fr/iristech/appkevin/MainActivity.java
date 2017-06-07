70f9f004e8f1cb4b7106dd613fa688c20e113b22
Tristan Wagner <t.wagner66250@gmail.com>
2017-06-07 10:29:47 +0200

    Requête JSON

--- app/src/main/java/fr/iristech/appkevin/MainActivity.java
+++ app/src/main/java/fr/iristech/appkevin/MainActivity.java
@@ -7,36 +7,62 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    RequestQueue queue;
     Button button;
-    EditText editText;
-    String editTextContent;
+    TextView textView;
+    String pseudo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText) findViewById(R.id.editText);
+        queue = Volley.newRequestQueue(this);
+
+        textView = (TextView) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
 
         button.setOnClickListener(this);
 
     }
 
+    public void getAstros(){
+        String url = "https://owapi.net/api/v3/u/"+ pseudo +"/stats";
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                textView.setText(response);
+                Log.e("MainActivity",response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this,error.getMessage().toString(),Toast.LENGTH_SHORT).show();
+                textView.setText(error.getMessage().toString());
+            }
+        });
+        queue.add(request);
+    }
+
     @Override
     public void onClick(View v) {
         if (v == button){
-            if (!editText.getText().toString().isEmpty()){
-                editTextContent = editText.getText().toString();
-                Log.e("MainActivity",editTextContent);
-                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
-                intent.putExtra("text",editTextContent);
-                startActivity(intent);
-            }
+            getAstros();
         }
     }
 }
